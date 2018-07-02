e2de81022dedd0389bffe22b152ccf25140c9507
Hanbi Hans Choi <personabe1984@gmail.com>
2018-07-02 05:26:56 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,49 @@
+def using_push(arr, element)
+  arr.push(element)
+end
+
+def using_unshift(arr, element)
+  arr.unshift(element)
+end
+
+def using_pop(arr)
+  arr.pop
+end
+
+def pop_with_args(arr)
+  return arr.pop(2)
+  
+end
+
+def using_shift(arr)
+  return arr.shift
+end 
+
+def shift_with_args(arr)
+  arr.shift(2)
+end
+
+def using_concat(arr1, arr2)
+  arr1.concat(arr2)
+end
+
+def using_insert(arr, element)
+  arr.insert(4, element)
+end
+
+def using_uniq(arr)
+  arr.uniq
+end
+
+def using_flatten(arr)
+  arr.flatten
+end
+
+def using_delete(arr, str)
+  arr.delete(str)
+end 
+
+def using_delete_at(arr, index)
+  arr.delete_at(index)
+end
+
